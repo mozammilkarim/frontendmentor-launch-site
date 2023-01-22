a464ce1d4c391bdb828feb5df9d3007e76880a75
mozammilkarim <mozammilkarim@gmail.com>
2023-01-22 10:13:53 +0530

    demo deployed

--- src/components/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PrimaryButton = () => {
-  return (
-    <div className='cursor-pointer text-center sm:text-start w-[320px] sm:w-fit font-[800] py-3 px-[18px] hover:text-white border-2 hover:bg-[#093F68] rounded-full text-[#093F68] bg-[#3EE9E5] border-[#3EE9E5]'>Get notified</div>
-  )
-}
-
-export default PrimaryButton
